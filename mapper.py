d12aa664b9dc69bd5bdc5282df4d81fcf41330ee
ashrithsridhar <ashrith.sridhar>
2019-12-01 23:11:42 +0530

    Hallelujah.

--- mapper.py
+++ mapper.py
@@ -2,7 +2,7 @@ import sys
 
 
 rough_schema = {'type': 'load', 'database': 'bigdata', 'csv_file_name': 'project_list.csv', 'column_types': [{'name': 'batsman', 'datatype': 'string'}, {'name': 'bowler', 'datatype': 'string'}, {'name': 'wickets', 'datatype': 'integer'}, {'name': 'runs', 'datatype': 'integer'}], 'aggregate': 'avg', 'agg_columns': 'student_name'}
-query = {'type': 'select', 'aggregate': None, 'agg_columns': None, 'columns': ['batsman'], 'clauses': {'and': ['wickets = 5', 'runs = 29'], 'or': []}}
+query = {'type': 'select', 'aggregate': None, 'agg_columns': None, 'columns': ['batsman'], 'clauses': {'and': [], 'or': []}}
 
 
 #'wickets = 5', 'runs = 29'
@@ -17,25 +17,26 @@ get_col_nums_list, get_col_nums_list1, get_col_nums_list2 = [], [], []
 #key, value = [], []
 
 
-def get_column_num(query_cols, schema_cols, aggregate, query_and):    
-    if aggregate == 0: #Get what column numbers SELECT will take
+def get_column_num(query_cols, schema_cols, aggregate, query_and) :    
+    if aggregate == 0:
         for i in range(len(query_cols)):
             a = schema_cols.index(str(query_cols[i]))
             get_col_nums.update({query_cols[i] : int(a)})
             get_col_nums_list.append(int(a))
         return get_col_nums
-    if aggregate == 1: #Get what column numbers aggregate will take
-        a = int(schema_cols.index(query_agg))
+    if aggregate == 1:
+        a = int(schema_cols.index(query['agg_columns']))
         get_col_nums_list1.append(a)
         get_col_nums1.update({'aggregate' : a})
         return get_col_nums1
-    if aggregate == 2: #Get what column numbers and their values WHERE will take
-        for i in range(len(query_and)): 
-            want = [query_and[i].split(' ') for i in range(len(query_and))]
+    if aggregate == 2:
+        for i in range(len(query['clauses']['and'])):
+            want = [ query['clauses']['and'][i].split(' ') for i in range(len(query['clauses']['and'])) ]
             a = schema_cols.index(str(want[i][0]))
             get_col_nums2.update({want[i][0] : [int(a), int(want[i][2]), str(want[i][1])]})
             get_col_nums_list2.append([int(a), int(want[i][2])])
         return get_col_nums2
+
 
 '''def purify(l):
     return [i for i in l if not i & 1]'''
@@ -81,39 +82,35 @@ else:
 
         for i in range(len(list(get_column_num(query_cols, schema_cols, 2, query_and).values()))):
             #print(int(line[int(c[i][1][0])]), c[i][1][1])
-            x = int(line[(c[i][1][0])])
+            '''x = int(line[(c[i][1][0])])
             x = [True if int(line[(c[i][1][0])]) == c[i][1][1] else False for i in range(len(c))]
-            print(x)
-
+            print(x)'''
 
 
             if c[i][1][-1] == '=' and int(line[int(c[i][1][0])]) == c[i][1][1]:
                 for i in range(len(list(get_column_num(query_cols, schema_cols, 0, query_and).values()))):
                     #print("\t",line[int(get_col_nums_list[i])])
                     select_columns[i].append(line[int(get_col_nums_list[i])])
-                    print()
             if c[i][1][-1] == '>' and int(line[int(c[i][1][0])]) < c[i][1][1]:
                 for i in range(len(list(get_column_num(query_cols, schema_cols, 0, query_and).values()))):
                     select_columns[i].append(line[int(get_col_nums_list[i])])
             if c[i][1][-1] == '<' and int(line[int(c[i][1][0])]) > c[i][1][1]:
                 for i in range(len(list(get_column_num(query_cols, schema_cols, 0, query_and).values()))):
                     select_columns[i].append(line[int(get_col_nums_list[i])])
-            print()
-            print()
-        print('===')    
 
 
-    if(query['aggregate'] is None):
+
+    '''if(query['aggregate'] is None):
         key = select_columns
-        value = [ query['type'], get_column_num(query_cols, schema_cols, 0, query_and), c]
+        value = [ query['type'], get_column_num(query_cols, schema_cols, 0, query_and), c ]
         key, value = str(key), str(value)          
-        print('%s:%s' % (key, value))
-    
-    if(query['aggregate'] is not None):
-        key = select_columns
-        value = [ query['type'], get_column_num(query_cols, schema_cols, 0, query_and), c]
-        key, value = str(key), str(value)            
-        print('%s:%s' % (key, value))
+        print('%s:%s' % (key, value))'''
+
+
+    key = select_columns
+    value = [ query['type'], get_column_num(query_cols, schema_cols, 0, query_and), c]
+    key, value = str(key), str(value)            
+    print('%s:%s' % (key, value))
 
     
 
