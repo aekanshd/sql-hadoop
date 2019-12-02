2989cedee08c10174563f7493bd727bd86fa2b44
ashrithsridhar <ashrith.sridhar>
2019-12-02 08:35:43 +0530

    Reducer is added.

--- mapper.py
+++ mapper.py
@@ -1,12 +1,12 @@
 import sys
 import json
 
-#rough_schema = {'type': 'load', 'database': 'bigdata', 'csv_file_name': 'project_list.csv', 'column_types': [{'name': 'batsman', 'datatype': 'string'}, {'name': 'bowler', 'datatype': 'string'}, {'name': 'wickets', 'datatype': 'integer'}, {'name': 'runs', 'datatype': 'integer'}], 'aggregate': 'avg', 'agg_columns': 'student_name'}
-#query = {'type': 'select', 'aggregate': None, 'agg_columns': None, 'columns': ['batsman'], 'clauses': {'and': [], 'or': []}}
+rough_schema = {'type': 'load', 'database': 'bigdata', 'csv_file_name': 'project_list.csv', 'column_types': [{'name': 'batsman', 'datatype': 'string'}, {'name': 'bowler', 'datatype': 'string'}, {'name': 'wickets', 'datatype': 'integer'}, {'name': 'runs', 'datatype': 'integer'}], 'aggregate': 'avg', 'agg_column': 'student_name'}
+query = {'type': 'select', 'aggregate': None, 'agg_column': None, 'columns': ['batsman', 'bowler'], 'clauses': {'or': ['wickets = 5', 'runs = 29'], 'and': []}}
 
 
-rough_schema = json.loads(sys.argv[1])
-query = json.loads(sys.argv[2])
+#rough_schema = json.loads(sys.argv[1])
+#query = json.loads(sys.argv[2])
 
 
 #'wickets = 5', 'runs = 29'
@@ -15,6 +15,7 @@ schema_cols = [rough_schema['column_types'][i]['name'] for i in range(0,len(roug
 query_cols = query['columns']
 query_and = query['clauses']['or']
 query_agg = query['agg_column']
+agg_list = []    
 get_col_nums, get_col_nums1, get_col_nums2 = {}, {}, {}
 get_col_nums_list, get_col_nums_list1, get_col_nums_list2 = [], [], []
 #get_col_nums = [get_col_nums.append(int(schema_cols.index(str(query_cols[i])))) for i in range(len(query_cols))]
@@ -29,17 +30,19 @@ def get_column_num(query_cols, schema_cols, aggregate, query_and) :
             get_col_nums_list.append(int(a))
         return get_col_nums
     if aggregate == 1:
-        a = int(schema_cols.index(query['agg_columns']))
+        a = int(schema_cols.index(query['agg_column']))
         get_col_nums_list1.append(a)
         get_col_nums1.update({'aggregate' : a})
         return get_col_nums1
     if aggregate == 2:
-        for i in range(len(query['clauses']['and'])):
-            want = [ query['clauses']['and'][i].split(' ') for i in range(len(query['clauses']['and'])) ]
+        for i in range(len(query['clauses']['or'])):
+            want = [ query['clauses']['or'][i].split(' ') for i in range(len(query['clauses']['or'])) ]
             a = schema_cols.index(str(want[i][0]))
             get_col_nums2.update({want[i][0] : [int(a), int(want[i][2]), str(want[i][1])]})
             get_col_nums_list2.append([int(a), int(want[i][2])])
         return get_col_nums2
+
+
 
 
 '''def purify(l):
@@ -52,19 +55,11 @@ select_columns = [[] for i in range(len(query_cols))]
 
 #Get WHERE column names and list of position with value : [('year', [1, 2]), ('cgpa', [2, 3])]
 c = sorted(get_column_num(query_cols, schema_cols, 2, query_and).items(), key = lambda x: x[1])
-print(c)
-"""
-| col1 | col2 | col3 |
-1,2,3
-3,2,1
-1,1,1
+#print(c)
 
-
-[[1,3,1], [2,2,1]]
-"""
 
 #Check for PROJECT query. Enter this loop if true.            
-if len(query['clauses']['and']) == 0 and query['aggregate'] is None: 
+if len(query['clauses']['or']) == 0 and query['aggregate'] is None: 
     for line in sys.stdin:
         line = line.strip().split(',')
         #print(line)
@@ -72,29 +67,54 @@ if len(query['clauses']['and']) == 0 and query['aggregate'] is None:
         for i in range(len(list(get_column_num(query_cols, schema_cols, 0, query_and).values()))):
             select_columns[i].append(line[int(get_col_nums_list[i])])
     
-    key = select_columns
-    a, b = get_column_num(query_cols, schema_cols, 0, query_and), get_column_num(query_cols, schema_cols, 2, query_and)
-    value = [query['type']]
-    key = str(key)
-    value = str(value)            
-    print('%s:%s' % (key, value))
+    key = query['columns']
+    value = str(select_columns)[1:-1]           
+    print('%s^%s' % (key, value))
 
-#check for SELECT and WHERE. Enter loop if true.
-else:
+#check for SELECT and WHeRE. Enter loop if true.
+if len(query['clauses']['or']) > 0 and query['aggregate'] is not None:
+    agg_column_num = schema_cols.index(query_agg)
     for line in sys.stdin:
         line = line.strip().split(',')
 
         for i in range(len(list(get_column_num(query_cols, schema_cols, 2, query_and).values()))):
-            #print(int(line[int(c[i][1][0])]), c[i][1][1])
-            '''x = int(line[(c[i][1][0])])
-            x = [True if int(line[(c[i][1][0])]) == c[i][1][1] else False for i in range(len(c))]
-            print(x)'''
+        
+            if c[i][1][-1] == '=' and int(line[int(c[i][1][0])]) == c[i][1][1]:
+                    agg_list.append(line[agg_column_num])
+            if c[i][1][-1] == '>' and int(line[int(c[i][1][0])]) < c[i][1][1]:
+                    agg_list.append(line[agg_column_num])
+            if c[i][1][-1] == '<' and int(line[int(c[i][1][0])]) > c[i][1][1]:
+                    agg_list.append(line[agg_column_num])
+    
+    key = [query['aggregate']]
+    value = agg_list
+    key, value = str(key), str(value)          
+    print('%s^%s' % (key, value))
 
+
+if len(query['clauses']['or']) == 0 and query['aggregate'] is not None:
+    agg_column_num = schema_cols.index(query_agg)
+    for line in sys.stdin:
+        line = line.strip().split(',')
+
+        agg_list.append(line[agg_column_num])
+    
+    key = [query['aggregate']]
+    value = agg_list
+    key, value = str(key), str(value)          
+    print('%s^%s' % (key, value))
+
+
+
+if len(query['clauses']['or']) > 0 and query['aggregate'] is None:
+    for line in sys.stdin:
+        line = line.strip().split(',')
+
+        for i in range(len(list(get_column_num(query_cols, schema_cols, 2, query_and).values()))):
 
             if c[i][1][-1] == '=' and int(line[int(c[i][1][0])]) == c[i][1][1]:
                 for i in range(len(list(get_column_num(query_cols, schema_cols, 0, query_and).values()))):
-                    #print("\t",line[int(get_col_nums_list[i])])
-                    select_columns[i].append(line[int(get_col_nums_list[i])])
+                        select_columns[i].append(line[int(get_col_nums_list[i])])
             if c[i][1][-1] == '>' and int(line[int(c[i][1][0])]) < c[i][1][1]:
                 for i in range(len(list(get_column_num(query_cols, schema_cols, 0, query_and).values()))):
                     select_columns[i].append(line[int(get_col_nums_list[i])])
@@ -103,22 +123,19 @@ else:
                     select_columns[i].append(line[int(get_col_nums_list[i])])
 
 
+    key = query['clauses']['or']
+    #value = select_columns
+    value = json.dumps(select_columns)
+    print(value)
+    value = json.loads(value)
+    print(value)
+    print(select_columns)
+    #value = json.loads(value)
+    #key, value = str(key), str(value)          
+    #print('%s^%s' % (key, value))
 
-    if(query['aggregate'] is None):
-        key = select_columns
-        value = [ query['type'], get_column_num(query_cols, schema_cols, 0, query_and), c ]
-        key, value = str(key), str(value)          
-        print('%s:%s' % (key, value))
-
-    if(query['aggregate'] is not None): 
-        key = select_columns
-        value = [ query['type'], query['agg_columns'], get_column_num(query_cols, schema_cols, 2, query_and)]
-        key, value = str(key), str(value)            
-        print('%s:%s' % (key, value))
 
     
-
-
     
 
 
---
+++ reducer.py
@@ -0,0 +1,7 @@
+import csv
+import sys
+
+empty = []
+x = ''
+for line in sys.stdin:
+    print(type(line))
